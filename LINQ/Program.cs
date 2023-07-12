595bd96b84e1fc41a67484278778ec141e44a68d
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-07-11 20:35:52 -0400

    Initial Commit, working through the requirements

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gameTitles = new List<string>
+                {
+                    "Super Mario",
+                    "Animal Crossing: New Horizons",
+                    "Final Fantasy VII Remake",
+                    "Minecraft",
+                    "Grand Theft Auto V"
+                };
+
+            string linqResult = VideoGameTitles(gameTitles);
+            Console.WriteLine(linqResult);
+
+
+
+        }
+
+        public static string VideoGameTitles(List<string> gameTitles) 
+        {
+            gameTitles.OrderByDescending(x => x).ToList();
+            return string.Join(", " , gameTitles) ;
         }
     }
 }
